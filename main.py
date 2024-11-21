e8afcfd57f206dde00753372a6f6bd51e8c091fd
Abel El Zachary <aelzachary@gmail.com>
2024-11-21 18:42:09 +0800

    menambahkan feature calculate product ke main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #import module
-#apply fungsi
+from feature.calculate_product import calculate_product
+
+#testing
+a = 10
+b = 5
+product_result = calculate_product(a,b)
+
+print(f'Product: {product_result}')
