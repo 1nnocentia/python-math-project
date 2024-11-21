18ecbddea47b9267d0266dac83533334d912582a
Fernandes Howard <fernandeshoward2005@gmail.com>
2024-11-21 18:35:08 +0800

    Memasukkan feature calculate difference ke dalam main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #import module
+from feature.calculate_difference import calculate_difference
 #apply fungsi
+a = 10
+b = 5
+
+difference_result = calculate_difference(a,b)
+
+print(f'Difference: {difference_result}')
